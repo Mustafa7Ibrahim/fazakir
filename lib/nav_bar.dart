a4ed031d7a8a20bfbd4b9aa238cc7436d5509e7d
I7Mustafa <mustafa7ibrahim@gmail.com>
2022-03-26 17:59:24 +0200

    add nav

---
+++ lib/nav_bar.dart
@@ -0,0 +1,78 @@
+import 'package:fazakir/views/home/home.dart';
+import 'package:fazakir/views/prayer_times/prayer_times.dart';
+import 'package:fazakir/views/quran/soura_list.dart';
+import 'package:flutter/material.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({Key? key}) : super(key: key);
+  static const routeName = '/NavBar';
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int index = 0;
+
+  final screens = [
+    const Home(),
+    const SouraList(),
+    const PrayerTime(),
+    Container(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(children: screens, index: index),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: Theme.of(context).colorScheme.primary,
+          labelTextStyle: MaterialStateProperty.all(
+            const TextStyle(fontSize: 14.0),
+          ),
+        ),
+        child: NavigationBar(
+          selectedIndex: index,
+          onDestinationSelected: (currentIndex) => setState(
+            () => index = currentIndex,
+          ),
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.home_outlined),
+              selectedIcon: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              label: "فذكر",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.menu_book_rounded),
+              selectedIcon: Icon(
+                Icons.menu_book_rounded,
+                color: Colors.white,
+              ),
+              label: "القرءان",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.mosque_outlined),
+              selectedIcon: Icon(
+                Icons.mosque,
+                color: Colors.white,
+              ),
+              label: "الأذان",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings_outlined),
+              selectedIcon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              label: "الأعدادات",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
